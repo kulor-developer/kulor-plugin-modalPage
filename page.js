874fd35394e835d08029e8b4db7b46c4ae5c04b0
bo.cheng <chengbo@QXIT-PC-000106.local>
2016-03-01 15:00:00 +0800

    update word

--- page.js
+++ page.js
@@ -1,20 +1,20 @@
 define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , function( Base , Template , RequireFile , ModalView ){
     "use strict"
     /*!
-     *	富客户端 提供一个page管理
-     * 	@pageListId 	{string} 	自定义一个pageListId
+     *  富客户端 提供一个page管理
+     *  @pageListId     {string}    自定义一个pageListId
      */
-    var PageList 	= Base.extend( function( pageListId , $pageContainer , opt ){
-        this._pageListConfig 	= {
-            pageListId 		: pageListId ,
-            urlHistroy 		: [] ,
-            $container 		: 0 ,
-            originOpt 		: $.extend( {
-                beforeDisplayEvents 	: [] ,
-                afterDisplayEvents 		: [] 
+    var PageList    = Base.extend( function( pageListId , $pageContainer , opt ){
+        this._pageListConfig    = {
+            pageListId      : pageListId ,
+            urlHistory      : [] ,
+            $container      : 0 ,
+            originOpt       : $.extend( {
+                beforeDisplayEvents     : [] ,
+                afterDisplayEvents      : [] 
             } , opt )
         };
-        this.length 	= 0;
+        this.length     = 0;
         if ( $pageContainer.length ) {
             this.initPageContainer( $pageContainer );
         }
@@ -25,42 +25,42 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
         this.addPage( opt );
         this.__pageListConfig.pageItems.push( this );
     } , {
-        implements 		: [ new Template() , new RequireFile() ],
-        __pageListConfig 	: {
+        implements      : [ new Template() , new RequireFile() ],
+        __pageListConfig    : {
             templateIds : {
-                container 	: "pageContainerTemplate"
+                container   : "pageContainerTemplate"
             } ,
-            pageItems 	: []
+            pageItems   : []
         } ,
         /*!
-         *	初始化page页面
-         * 	@$container 	{JQDOM}
+         *  初始化page页面
+         *  @$container     {JQDOM}
          */
-        initPageContainer 	: function( $container ){
+        initPageContainer   : function( $container ){
             this._pageListConfig.$container = $container;
             $container.addClass( "uiSub-page-container" );
-            // 	暂留提供page主题相关的引入
-            // 	$container.html( this.getTemplate( this.__pageListConfig.templateIds.container) );
+            //  暂留提供page主题相关的引入
+            //  $container.html( this.getTemplate( this.__pageListConfig.templateIds.container) );
             return this;
         } ,
-        addPage 		: function( pages ){
+        addPage         : function( pages ){
             for( var a in pages ){
                 this[ a ] = new Page( this , pages[ a ] , a );
             }
             return this;
         } ,
-        addBeforeDisplayEvent 	: function( funcs ){
-            funcs 	= funcs instanceof Array ? funcs : [ funcs ];
+        addBeforeDisplayEvent   : function( funcs ){
+            funcs   = funcs instanceof Array ? funcs : [ funcs ];
             this._pageListConfig.originOpt.beforeDisplayEvents = this._pageListConfig.originOpt.beforeDisplayEvents.concat( funcs );
             return this;
         } ,
-        addAfterDisplayEvent 	: function( funcs ){
-            funcs 	= funcs instanceof Array ? funcs : [ funcs ];
+        addAfterDisplayEvent    : function( funcs ){
+            funcs   = funcs instanceof Array ? funcs : [ funcs ];
             this._pageListConfig.originOpt.afterDisplayEvents = this._pageListConfig.originOpt.afterDisplayEvents.concat( funcs );
             return this;
         } ,
-        handleDisplayEvent 	: function( pageName , type ){
-            var _events 	= this._pageListConfig.originOpt[ type + "Events" ];
+        handleDisplayEvent  : function( pageName , type ){
+            var _events     = this._pageListConfig.originOpt[ type + "Events" ];
             if( _events ){
                 for( var i = 0 , len = _events.length; i < len; i++ ){
                     if( typeof _events[ i ] === "function" ){
@@ -72,47 +72,47 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
             }
             return this;
         } ,
-        urlHistroyBack 	: function( backNum ){
-            var _config 	= this._pageListConfig;
-            backNum = _config.urlHistroy.length - ( backNum || 1 );
+        urlHistoryBack  : function( backNum ){
+            var _config     = this._pageListConfig;
+            backNum = _config.urlHistory.length - ( backNum || 1 );
             if( backNum >= 0) {
-                this[ _config.urlHistroy[ backNum - 1 ] || this._pageListConfig.originOpt.defaultHistroyPage ].display( function(){
-                    _config.urlHistroy = _config.urlHistroy.slice( 0 , backNum );
+                this[ _config.urlHistory[ backNum - 1 ] || this._pageListConfig.originOpt.defaultHistroyPage ].display( function(){
+                    _config.urlHistory = _config.urlHistory.slice( 0 , backNum );
                 } );
             }
             return this;
         } 
     } ) ,
         /*!
-         *	单个page模块
+         *  单个page模块
          */
-        Page 	= Base.extend( function( belongPageList , jsPath , pageName ){
-            this._pageConfig 	= {
-                pageName 	: pageName ,
-                $container 	: false ,
-                ready 		: false ,
-                jsPath 		: jsPath ,
-                eventList 	: [] ,
-                belongPageList 	: 	belongPageList
+        Page    = Base.extend( function( belongPageList , jsPath , pageName ){
+            this._pageConfig    = {
+                pageName    : pageName ,
+                $container  : false ,
+                ready       : false ,
+                jsPath      : jsPath ,
+                eventList   : [] ,
+                belongPageList  :   belongPageList
             }
             belongPageList.length++;
         } , {
             __pageConfig: {
-                templateIds 	: {
-                    container 	: "subPageContainerTemplate"
+                templateIds     : {
+                    container   : "subPageContainerTemplate"
                 }
             } ,
-            display 	: function(){
+            display     : function(){
                 return this.displayPageModal.apply( this , arguments );
             } ,
-            get 		: function(){
+            get         : function(){
                 return this.getPageModal.apply( this , arguments );
             } ,
-            ready 		: function(){
+            ready       : function(){
                 return this.setPageToReady.apply( this , arguments );
             } ,
-            displayPageModal	: function( func ){
-                var _self 		= this ,
+            displayPageModal    : function( func ){
+                var _self       = this ,
                     _$container = this._pageConfig.belongPageList._pageListConfig.$container;
                 _self._pageConfig.belongPageList.handleDisplayEvent( _self._pageConfig.pageName , "beforeDisplay" );
                 _self.getPageModal( function(){
@@ -123,7 +123,7 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
                 } );
                 return this;
             } ,
-            handlePageEventList 	: function(){
+            handlePageEventList     : function(){
                 // 转移事件队列，将队列池清空，防止队列中事件互调造成内存溢出
                 var _el = [].concat( this._pageConfig.eventList );
                 this._pageConfig.eventList.length = 0;
@@ -133,8 +133,8 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
                 _el = null;
                 return this;
             } ,
-            getPageModal 		: function( func ){
-                this._pageConfig.belongPageList._pageListConfig.urlHistroy.push( this._pageConfig.pageName );
+            getPageModal        : function( func ){
+                this._pageConfig.belongPageList._pageListConfig.urlHistory.push( this._pageConfig.pageName );
                 this._pageConfig.eventList.push( function(){
                     if( $.isFunction( func ) && 
                         ( $.isFunction( this._pageConfig.belongPageList._pageListConfig.callBack ) ? this._pageConfig.belongPageList._pageListConfig.callBack.call( this ) !== false : true )
@@ -147,15 +147,15 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
                     this._pageConfig.ready = "waiting";
                 }
                 return this;
-            } ,	
+            } , 
             /*!
-             *	绘制page的内容
-             * 	@templateId 	{string} 	
-             */		
-            getPageModalUI 		: function( templateId ){
+             *  绘制page的内容
+             *  @templateId     {string}    
+             */     
+            getPageModalUI      : function( templateId ){
                 var _$container = this._pageConfig.belongPageList._pageListConfig.$container;
                 if( !this._pageConfig.$container ){
-                    this._pageConfig.$container 	= $( this._pageConfig.belongPageList.getTemplate( this.__pageConfig.templateIds.container ) );
+                    this._pageConfig.$container     = $( this._pageConfig.belongPageList.getTemplate( this.__pageConfig.templateIds.container ) );
                     _$container.append( this._pageConfig.$container );
                 }
                 if( templateId ){
@@ -163,7 +163,7 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
                 }
                 return this._pageConfig.$container;
             } ,
-            setPageToReady 	: function( pageInfo , constructor ){
+            setPageToReady  : function( pageInfo , constructor ){
                 var _self = this ,
                     _done = function(){
                         _self._pageConfig.ready = "ready";
@@ -173,10 +173,10 @@ define( "ModalPage" , [ "Base" , "Template" , "RequireFile" , "ModalView" ] , fu
                     };
                 if ( typeof pageInfo == "function" ) {
                     constructor = pageInfo;
-                    pageInfo 	= {};
+                    pageInfo    = {};
                 }
                 if ( pageInfo.requireFile instanceof Array ) {
-                    _self._pageConfig.belongPageList.getFile( pageInfo.requireFile , _done );	
+                    _self._pageConfig.belongPageList.getFile( pageInfo.requireFile , _done );   
                 } else {
                     _done();
                 }
